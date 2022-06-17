ccc1272af76c003d742100f168fedd2c2047073e
Asael Silva <asael.guitar_g3@hotmail.com>
2022-06-17 10:23:17 -0300

    Class funcionario

---
+++ 01 - Testes de Unidade/Demo/Demo/FuncionarioFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    public class FuncionarioFactory
+    {
+        public static Funcionario Criar(string nome, double salario)
+        {
+            return new Funcionario(nome, salario);
+        }
+    }
+}
