4b20d21d51b9fc655e989811dd80eaef8bfa50b2
Asael Silva <asael.guitar_g3@hotmail.com>
2022-06-21 14:38:44 -0300

    Organizando pastas

---
+++ 01 - Testes de Unidade/Features.Tests/02 - Fixtures/ClienteTestsFixtures.cs
@@ -0,0 +1,44 @@
+﻿using Features.Clientes;
+using Xunit;
+
+namespace Features.Tests._02___Fixtures
+{
+    [CollectionDefinition(nameof(ClienteCollection))]
+    public class ClienteCollection : ICollectionFixture<ClienteTestsFixture>
+    { }
+
+    public class ClienteTestsFixture : IDisposable
+    {
+        public Cliente GerarClienteValido()
+        {
+            var cliente = new Cliente(
+                Guid.NewGuid(),
+                "Asael",
+                "Silva",
+                DateTime.Now.AddYears(-30),
+                "Asael.silva@hotmail.com",
+                true,
+                DateTime.Now);
+
+            return cliente;
+        }
+
+        public Cliente GerarClienteInValido()
+        {
+            var cliente = new Cliente(
+                Guid.NewGuid(),
+                "",
+                "",
+                DateTime.Now,
+                "asael.silva.com",
+                true,
+                DateTime.Now);
+
+            return cliente;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
