f7866e46ff2660896a2d1e7dcdab21f03d83d286
Asael Silva <asael.guitar_g3@hotmail.com>
2022-06-21 17:36:36 -0300

    TDD

---
+++ 02 - TDD/src/NerdStore.Vendas.Domain/Pedido.cs
@@ -0,0 +1,63 @@
+﻿using NerdStore.Vendas.Domain.Enum;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdStore.Vendas.Domain
+{
+    public class Pedido
+    {
+        protected Pedido()
+        {
+            _pedidoItems = new List<PedidoItem>();
+        }
+        
+        public Guid ClienteId { get; private set; }
+        public decimal ValorTotal { get; private set; }
+        public PedidoStatus PedidoStatus { get; private set; }
+
+        private readonly List<PedidoItem> _pedidoItems;
+        public IReadOnlyCollection<PedidoItem> PedidoItems => _pedidoItems;
+
+        public void AdicionarItem(PedidoItem pedidoItem)
+        {
+            if (_pedidoItems.Any(x => x.PedidoItemId == pedidoItem.PedidoItemId))
+            {
+                var itemExistente = _pedidoItems.FirstOrDefault(x => x.PedidoItemId == pedidoItem.PedidoItemId);
+
+                itemExistente.AdicionarUnidades(pedidoItem.Quantidade);
+                pedidoItem = itemExistente;
+                _pedidoItems.Remove(itemExistente);
+            }
+            _pedidoItems.Add(pedidoItem);
+            CalcularValorPedido();
+        }
+
+        public void CalcularValorPedido()
+        {
+            ValorTotal = PedidoItems.Sum(i => i.CalcularValor());
+        }
+
+        public void TornarRascunho()
+        {
+            PedidoStatus = PedidoStatus.Rascunho;
+        }
+
+        public static class PedidoFactory
+        {
+            public static Pedido NovoPedidoRascunho(Guid clienteId)
+            {
+                var pedido = new Pedido
+                {
+                    ClienteId = clienteId,
+                };
+
+                pedido.TornarRascunho();
+                return pedido;
+            }
+        }
+    }
+}
---
+++ 02 - TDD/src/NerdStore.Vendas.Domain/PedidoItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdStore.Vendas.Domain
+{
+    public class PedidoItem
+    {
+        public Guid PedidoItemId { get; private set; }
+        public string ProdutoNome { get; private set; }
+        public int Quantidade { get; private set; }
+        public decimal ValorUnitario { get; private set; }
+        public PedidoItem(Guid id, string descricao, int quantidade, decimal valor)
+        {
+            PedidoItemId = id;
+            ProdutoNome = descricao;
+            Quantidade = quantidade;
+            ValorUnitario = valor;
+        }
+
+        internal void AdicionarUnidades(int unidades)
+        {
+            Quantidade += unidades;
+        }
+
+        internal decimal CalcularValor()
+        {
+            return Quantidade * ValorUnitario;
+        }
+    }
+}
